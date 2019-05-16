448ab8704dcc464808dd5463ddc01e730a187bb5
declarative-banner-0824 <faxri96@live.com>
2019-05-16 15:15:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,15 +14,15 @@ end
 
 def add_element_to_start_of_array(array, element)
   
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
